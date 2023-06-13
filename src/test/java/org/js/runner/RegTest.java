ec3e3fe4fa3886a26f565774358eaf5f5d4e3e4f
BayAzat <azatbay.job@gmail.com>
2023-06-13 11:16:14 +0500

    1st commit

---
+++ src/test/java/org/js/runner/RegTest.java
@@ -0,0 +1,23 @@
+package org.js.runner;
+
+import org.js.model.MainPage;
+import org.js.runner.order.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RegTest extends BaseTest {
+
+    @Test
+    public void clickRegister() {
+
+        MainPage mainPage = new MainPage(getDriver())
+                .clickRegister();
+
+        WebElement textRegister =  getDriver().findElement(By.xpath("//h1[normalize-space()='Register']"));
+
+        Assert.assertEquals(textRegister.getText(),"Register");
+
+    }
+}
